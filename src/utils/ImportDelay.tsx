a1041b83db09c2cc167d0cf1365dbc5be8077dac
mouhciner0 <rachidmouhcine88@gmail.com>
2025-02-10 04:12:50 +0100

    dynamic path solved

--- src/utils/ImportDelay.tsx
+++ src/utils/ImportDelay.tsx
@@ -3,7 +3,7 @@ import { lazy } from "react";
 export default function ImportDelay(path: string) {
 	return lazy(() => {
 		return new Promise(resolve => {
-			setTimeout(() => resolve(import(path)), 700)
+			setTimeout(() => resolve(import(/* @vite-ignore */path)), 700)
 		})
 	})
 }
